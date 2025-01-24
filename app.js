aa4aa88e0e9df30be35a7426791ca6d4911ae827
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-23 17:28:03 -0800

    learning how to use emitter and encapsulation

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const EventEmitter = require('events'); //class
 const emitter = new EventEmitter(); //object
 
 //Register a listener
-emitter.on('messagedLogged', function(){
-    console.log('Listener called');
+emitter.on('messagedLogged', (arg) => {
+    console.log('Listener called', arg);
+});
+
+//Raise an event
+emitter.emit('messagedLogged', {id: 1, url: 'http://'});
+
+//Raise an event called logging, send data: message
+emitter.on('Logging', (arg) => {
+    console.log('Logging data: ', arg)
 })
-emitter.emit('messagedLogged');
+emitter.emit('Logging',{data: 'message'});
