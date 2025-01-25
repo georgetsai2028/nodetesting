c3da71a206366bb7149101d5ca45991b299ed3cc
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-25 12:27:44 -0800

    created basic server that can send to http

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    console.log('Here');
+    res.json({ message: "Error" });
+}) 
+
+
+app.listen(3000);
