545b27104323b323c823a9432dee2f438b1b8618
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-26 11:42:26 -0800

    learning how to parse from json data

--- server.js
+++ server.js
@@ -2,19 +2,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"))
 app.set ("view engine", "ejs")
-app.use(logger);
 
-app.get('/', logger, (req, res) => {
-    res.render("index", {text : "World"})
-}) 
 
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
 
-function logger(req, res, next){
-    console.log(req.originalUrl);
-    next();
-}
+
 app.listen(3000);
