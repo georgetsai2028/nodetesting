335ff0b20cf3619997f5f549dfa1b752497266d5
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-26 10:39:20 -0800

    created chain routing

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,16 @@ router.post('/', (req, res) => {
     res.send('Create User');
 })
 
-router.get('/:id', (req, res) => {
+router
+.route("/:id")
+.get((req, res) => {
     res.send(`Get User With ID ${req.params.id}`);
 })
+.put((req, res) => {
+    res.send(`Update User with ID ${req.params.id}`);
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`);
+})
+
 module.exports = router
