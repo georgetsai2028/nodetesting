37cb4712088df54d3352e743476d599a1bf53663
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-26 14:44:01 -0800

    learn how to parse query params

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,17 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    res.send('Create User');
+    const isValid = true;
+    if (isValid) {
+        users.push({firstName : req.body.firstName})
+        res.redirect(`/users/${users.length - 1}`)
+    }
+    else{
+        console.log("Error");
+        res.render('users/new', { firstName: req.body.firstName})
+    }
+    console.log(req.body.firstName)
+    res.send("hi")
 })
 
 router
