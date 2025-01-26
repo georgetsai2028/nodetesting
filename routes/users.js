7cd1310d945cc41371e6ae668339248995040238
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-26 10:33:38 -0800

    learning dynamic

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,11 @@ router.get('/new', (req, res) => {
     res.send('user new form');
 })
 
+router.post('/', (req, res) => {
+    res.send('Create User');
+})
+
+router.get('/:id', (req, res) => {
+    res.send(`Get User With ID ${req.params.id}`);
+})
 module.exports = router
